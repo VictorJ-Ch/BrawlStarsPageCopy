8285c741fdde29309ea35273845a77487e5be153
VictorJ-Ch <chavarria.martinez.victor101@gmail.com>
2023-10-30 20:25:59 -0600

    Practica en PHP

---
+++ docs/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Brawl Stars x Supercell</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="./CSS/Index.css">
+        <link rel="icon" type="image/png" href="https://supercell.com/_next/static/media/supercell_logo.38ec5826.webp">
+    </head>
+    <body>
+        <h1>Iniciar Sesión</h1>
+        <form method="POST" action="home.php">
+            <label for="user">user:</label>
+            <input type="text" name="user" id="user"/>
+            <br />
+            <input type="submit" value="Iniciar" />
+        </form>
+    </body>
+    <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
